097290bdbc631c895ace35acfe9429cdbcf8db79
João Salles <joao.sallesr@gmail.com>
2026-04-28 15:26:53 -0300

    partial readers x writers

--- 03-leitores-escritores/leit_escr.c
+++ 03-leitores-escritores/leit_escr.c
@@ -1,6 +1,93 @@
 #include "leit_escr.h"
 
+sem_t mutex;
+sem_t writers_sem;
+int readers_count = 0;
+
+int shared[SHARED_SIZE];
+
 int main() {
-    int i = 0;
+    // ESCRITA SEM PROTEÇÃO
+    printf("========== READERS x WRITERS - NO PROTECTION ==========\n\r");
+    sem_init(&mutex, 0, 1);
+    sem_init(&writers_sem, 0, 1);
+
+    pthread_t reader[READERS];
+    pthread_t writer[WRITERS];
+
+    for (int i = 0; i < READERS; i++)
+        pthread_create(&reader[i], NULL, &reader_pure, NULL);
+
+    for (int i = 0; i < WRITERS; i++)
+        pthread_create(&writer[i], NULL, &writer_pure, NULL);
+
+    for (int i = 0; i < READERS; i++)
+        pthread_join(reader[i], NULL);
+
+    for (int i = 0; i < WRITERS; i++)
+        pthread_join(writer[i], NULL);
+
+    sem_destroy(&mutex);
+    sem_destroy(&writers_sem);
+
+    // ESCRITA COM PROTEÇÃO
+    printf("========== READERS x WRITERS - PROTECTED ==========\n\r");
+    readers_count = 0;
+    sem_init(&mutex, 0, 1);
+    sem_init(&writers_sem, 0, 1);
+
+    for (int i = 0; i < READERS; i++)
+        pthread_create(&reader[i], NULL, &reader_protected, NULL);
+
+    for (int i = 0; i < WRITERS; i++)
+        pthread_create(&writer[i], NULL, &writer_protected, NULL);
+
+    for (int i = 0; i < READERS; i++)
+        pthread_join(reader[i], NULL);
+
+    for (int i = 0; i < WRITERS; i++)
+        pthread_join(writer[i], NULL);
+
+    sem_destroy(&mutex);
+    sem_destroy(&writers_sem);
+
     return 0;
+}
+
+void *reader_pure(void *ptr) {
+    readers_count++;
+    int a = 0;
+
+    for (int i = 0; i < SHARED_SIZE; i++)
+        a = shared[i];
+
+    readers_count--;
+}
+
+void *reader_protected(void *ptr) {
+    sem_wait(&mutex);
+    readers_count++;
+    if (readers_count == 1)
+        sem_wait(&writers_sem);
+    sem_post(&mutex);
+
+    int a = 0;
+    for (int i = 0; i < SHARED_SIZE; i++)
+        a = shared[i];
+
+    sem_wait(&mutex);
+    readers_count--;
+    if (readers_count == 0)
+        sem_post(&writers_sem);
+    sem_post(&mutex);
+}
+
+void *writer_pure(void *ptr) {
+    for (int i = 0; i < SHARED_SIZE; i++)
+        shared[i] == i;
+}
+void *writer_protected(void *ptr) {
+
+    for (int i = 0; i < SHARED_SIZE; i++)
+        shared[i] == i;
 }
--- 03-leitores-escritores/leit_escr.h
+++ 03-leitores-escritores/leit_escr.h
@@ -5,3 +5,12 @@
 #include <semaphore.h>
 #include <stdint.h>
 #include <stdio.h>
+
+#define READERS 5
+#define WRITERS 3
+#define SHARED_SIZE 20000
+
+void *reader_pure(void *ptr);
+void *reader_protected(void *ptr);
+void *writer_pure(void *ptr);
+void *writer_protected(void *ptr);
